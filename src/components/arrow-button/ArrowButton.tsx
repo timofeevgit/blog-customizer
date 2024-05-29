3635bcc7050525f9b9667c5e092b5d54b8ee04f8
Dmitry Timofeev <dimadimajob@gmail.com>
2024-05-29 20:26:46 +0300

    review

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,27 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { FC } from 'react';
+import { clsx } from'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface Props {
+	onClick: OnClick;
+	show: boolean;
+}
+
+export const ArrowButton: FC<Props> = ({onClick, show}) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			onClick={onClick}
+			className={clsx(styles.container, show && styles.container_open)}>
+			<img src={arrow} alt='иконка стрелочки' className={clsx(styles.arrow, show && styles.arrow_open)} />
 		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -13,19 +13,20 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [article, setArticle] = useState(defaultArticleState);
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': article.fontFamilyOption.value,
+					'--font-size': article.fontSizeOption.value,
+					'--font-color': article.fontColor.value,
+					'--container-width': article.contentWidth.value,
+					'--bg-color': article.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm params={article} onChange={setArticle} />
 			<Article />
 		</div>
 	);
